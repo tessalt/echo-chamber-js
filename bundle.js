5890dce7a6bcdc0235690381978ed2ea727ca5e5
Jenny Zhang <jzhang@commonsense.org>
2015-07-15 02:10:03 -0400

    Update EchoChamber.host to prod env

--- bundle.js
+++ bundle.js
@@ -606,7 +606,7 @@ var EchoChamber = (function (window, undefined) {
     prod: 'https://s3.amazonaws.com/echochamberjs/dist'
   }
 
-  EchoChamber.host = hosts.local;
+  EchoChamber.host = hosts.prod;
   EchoChamber.App = require('./src/echo_chamber.js'); 
 
   var app = Object.create(EchoChamber.App);
