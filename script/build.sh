10c1dc06d7603ade59f7f68de98ed251063520e5
Poliana Jardins <46983448+polianajardins@users.noreply.github.com>
2019-05-10 14:37:45 -0300

    Revert "fixed broken uglify build cmd"

--- script/build.sh
+++ script/build.sh
@@ -3,6 +3,5 @@ PATH=$(npm bin):$PATH
 if [[ ! -e dist ]]; then
     mkdir dist
 fi
-browserify entry.js > dist/main.js
-uglify -s dist/main.js -o dist/main.js
+browserify entry.js | uglify > dist/main.js
 cp src/main.css dist/main.css
